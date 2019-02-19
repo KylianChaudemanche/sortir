da4f8d913228bb1a800a760030d39d1c6adabdf8
CHAUDEMANCHE KYLIAN <kylian.chaudemanche2018@campus-eni.fr>
2019-02-19 14:12:11 +0100

    daoFactory

--- src/main/java/fr/eni/sortir/dao/DaoFactory.java
+++ src/main/java/fr/eni/sortir/dao/DaoFactory.java
@@ -1,10 +1,40 @@
 package fr.eni.sortir.dao;
 
 import fr.eni.sortir.dao.jpa.JpaEtatDao;
+import fr.eni.sortir.dao.jpa.JpaInscriptionDao;
+import fr.eni.sortir.dao.jpa.JpaLieuDao;
+import fr.eni.sortir.dao.jpa.JpaParticipantDao;
+import fr.eni.sortir.dao.jpa.JpaSiteDao;
+import fr.eni.sortir.dao.jpa.JpaSortieDao;
+import fr.eni.sortir.dao.jpa.JpaVilleDao;
 import fr.eni.sortir.utils.PersistenceManager;
 
 public class DaoFactory {
 	public static EtatDao getEtatDao() {
 		return new JpaEtatDao(PersistenceManager.getEntityManagerFactory());
+	}
+	
+	public static InscriptionDao getInscriptionDao() {
+		return new JpaInscriptionDao(PersistenceManager.getEntityManagerFactory());
+	}
+	
+	public static LieuDao getLieuDao() {
+		return new JpaLieuDao(PersistenceManager.getEntityManagerFactory());
+	}
+	
+	public static ParticipantDao getParticipantDao() {
+		return new JpaParticipantDao(PersistenceManager.getEntityManagerFactory());
+	}
+	
+	public static SiteDao getSiteDao() {
+		return new JpaSiteDao(PersistenceManager.getEntityManagerFactory());
+	}
+	
+	public static SortieDao getSortieDao() {
+		return new JpaSortieDao(PersistenceManager.getEntityManagerFactory());
+	}
+	
+	public static VilleDao getVilleDao() {
+		return new JpaVilleDao(PersistenceManager.getEntityManagerFactory());
 	}
 }
